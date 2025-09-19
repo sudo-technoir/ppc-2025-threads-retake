f8e35b87eb50eb6a74addc02b985335696984c0c
sudo-technoir <skurinskayae@gmail.com>
2025-09-19 21:19:06 +0300

    Update ops_seq.hpp

--- tasks/seq/shkurinskaya_e_convex_hull_components/include/ops_seq.hpp
+++ tasks/seq/shkurinskaya_e_convex_hull_components/include/ops_seq.hpp
@@ -6,6 +6,11 @@
 
 namespace shkurinskaya_e_convex_hull_components_seq {
 
+struct Point {
+  int x = 0;
+  int y = 0;
+};
+
 class ConvexHullSequential : public ppc::core::Task {
  public:
   explicit ConvexHullSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
