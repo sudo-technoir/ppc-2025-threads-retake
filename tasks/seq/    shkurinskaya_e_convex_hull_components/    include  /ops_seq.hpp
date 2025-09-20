5d975dd9f669b1e89abc66a5fef9bf8bdb373a33
sudo-technoir <skurinskayae@gmail.com>
2025-09-20 22:48:08 +0300

    Create ops_seq.hpp

---
+++ tasks/seq/    shkurinskaya_e_convex_hull_components/    include  /ops_seq.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace shkurinskaya_e_convex_hull_components_seq {
+
+struct Point {
+  int x = 0;
+  int y = 0;
+};
+
+class ConvexHullSequential : public ppc::core::Task {
+ public:
+  explicit ConvexHullSequential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  std::vector<Point> input_points_;
+  std::vector<Point> output_hull_;
+
+  static int Cross(const Point& o, const Point& a, const Point& b) noexcept;
+};
+
+}  // namespace shkurinskaya_e_convex_hull_components_seq
