0e105cc038d012bee081e382d8e4372327c1f605
sudo-technoir <skurinskayae@gmail.com>
2025-09-25 21:44:23 +0300

    Create ops_omp.hpp

---
+++ tasks/omp/shkurinskaya_e_convex_hull_components/include/ops_omp.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace shkurinskaya_e_convex_hull_components_omp {
+
+struct Point {
+  int x = 0;
+  int y = 0;
+};
+
+class ConvexHullOmp : public ppc::core::Task {
+ public:
+  explicit ConvexHullompuential(ppc::core::TaskDataPtr task_data) : Task(std::move(task_data)) {}
+  bool PreProcessingImpl() override;
+  bool ValidationImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+ private:
+  std::vector<Point> input_points_;
+  std::vector<Point> output_hull_;
+
+  static int Cross(const Point& o, const Point& a, const Point& b) noexcept;
+};
+
+}  // namespace shkurinskaya_e_convex_hull_components_omp
